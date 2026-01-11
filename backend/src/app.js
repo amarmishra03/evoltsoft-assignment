7947fd4e38f3e4f934bfa198a3dfcf8b5fd4db5c
amarmishra03 <109795692+amarmishra03@users.noreply.github.com>
2026-01-11 12:55:16 +0530

    deployment preparations

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth.routes"));
